f4d6b49ad0a38358683ebeac047394e81e6da370
Dualock <djfonsecamo@gmail.com>
2022-09-08 01:21:23 -0600

    Lab1 - Avance

--- Labs/Lab1/Lab1.py
+++ Labs/Lab1/Lab1.py
@@ -5,61 +5,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-'''def Relative_freq(filepath: str) -> list:
+def bar_graph(purchase_amount: list) -> None:
     """
-    Asignacion: 1
-    Calcula las frecuencias relativas en el archivo.csv
-    param filepath: direccion del archivo.csv
-    return: Una lista con la frecuencia relativa de compra
-    """
-    relative_frequencies = []
-    a_count, b_count, c_count, d_count = 0, 0, 0, 0
-    a, b, c, d = 0, 0, 0, 0
-    total = 0
-    with open(filepath, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in reader:
-            elements = row[0].split(',')
-            # ignore first line
-            if (elements[0] == "Cliente"):
-                pass
-            else:
-                # print('linea: {} A: {}'.format(elements[0],elements[1]))
-                a, b = int(elements[1]), int(elements[2])
-                c, d = int(elements[3]), int(elements[4])
-                total += 1
-                if (a):
-                    a_count += 1
-                if (b):
-                    b_count += 1
-                if (c):
-                    c_count += 1
-                if (d):
-                    d_count += 1
-                # print('count A = {} B = {}'.format(a_count, b_count))
-    relative_frequencies.append(a_count/total)
-    relative_frequencies.append(b_count/total)
-    relative_frequencies.append(c_count/total)
-    relative_frequencies.append(d_count/total)
-    print(relative_frequencies)
-    return relative_frequencies'''
-
-
-def bar_graph(cantidad_compras: list) -> None:
-    """
-    Asignacion: 2
-    Realiza un grafico de barras a partir de una lista
+    Asignación: 2
+    Realiza un gráfico de barras a partir de una lista
     de frecuencias relativas
-    param cantidad_compras: lista con la cantidad de compras por producto
+    param purchase_amout: lista con la cantidad de compras por producto
     """
-    # Agrega la lista de frecuencias relativas y los índices al dataframe
+    # Agrega la lista de frecuencias relativas y los índices a la figura
     index = ['A', 'B', 'C', 'D']
-    plt.bar(index, cantidad_compras)
+    plt.bar(index, purchase_amount)
     plt.title("Compras por producto")
     plt.xlabel("Producto")
     plt.ylabel("Cantidad de compras")
     # Se muestra la figura graficada
     plt.show()
+
 
 def relative_freq() -> list:
     """
@@ -75,8 +36,8 @@ def relative_freq() -> list:
     data_frame_b = pd.DataFrame(data, columns=['B'])
     data_frame_c = pd.DataFrame(data, columns=['C'])
     data_frame_d = pd.DataFrame(data, columns=['D'])
-    # Se calcula la frecuencia de aparicion de cada elemento
-    # Utilizando la funcion value_counts
+    '''Se calcula la frecuencia de aparición de cada elemento
+    utilizando la función value_counts'''
     # Se usa sort = false para no ordenarlos por cantidad de apariciones
     # Se utiliza normalize para calcular la frecuencia relativa directamente
     a_relative_f = data_frame_a['A'].value_counts(normalize=True, sort=False)
@@ -87,55 +48,83 @@ def relative_freq() -> list:
     b_count = data_frame_b['B'].value_counts(sort=False)
     c_count = data_frame_c['C'].value_counts(sort=False)
     d_count = data_frame_d['D'].value_counts(sort=False)
-    cantidad_compras = [a_count[1], b_count[1], c_count[1], d_count[1]]
-    print(cantidad_compras)
+    purchase_amount = [a_count[1], b_count[1], c_count[1], d_count[1]]
     # Las frecuencias relativas se reciben como un arreglo
     # a_relative_f[0] contiene la frecuencia relativa de la no compra(0)
     # a_relative_f[1] contiene la frecuencia relativa de la compra (1)
     relative_frequencies = \
         [a_relative_f[1], b_relative_f[1], c_relative_f[1], d_relative_f[1]]
-    return cantidad_compras
+    print("Frecuencias relativas - A: {} B: {} C: {} D: {}"
+          .format(relative_frequencies[0], relative_frequencies[1],
+                  relative_frequencies[2], relative_frequencies[3]))
+    return purchase_amount
 
 
 def combination_relative_freq(filepath: str) -> list:
     """
-    Asignacion: 3
-    Calcula las frecuencias relativas de cada combinacion de 
+    Asignación: 3
+    Calcula las frecuencias relativas de cada combinación de
     compra por cliente
     """
     # Cantidad de combinaciones
     combinations = 16
-    # Lista con la cantidad de apariciones de cada combinacion
-    frecuencias = [0]*combinations
-    
-    # Numeros enteros con la combinacion de productos comprados
-    a, b, c, d = 0, 0, 0, 0
-    # Num es un numero pensado en binario para indicar la combinacion
-    # donde A es el bit mas significativo y B el menos significativo
+    # Lista con la cantidad de apariciones de cada combinación
+    frequencies = [0]*combinations
+    # Números enteros con la combinación de productos comprados
+    producto_a, producto_b, producto_c, producto_d = 0, 0, 0, 0
+    '''Num es un número para indicar la combinación
+    donde A es el bit mas significativo y D el menos significativo'''
     num = 0
     # total es la cantidad de personas que compraron los productos
     total = 0
     with open(filepath, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        # Se lee cada fila en el csv
         for row in reader:
+            # se separan los elementos por ','
             elements = row[0].split(',')
-            # ignore first line
+            # ignoramos la primer línea
             if (elements[0] == "Cliente"):
                 pass
             else:
-                # print('linea: {} A: {}'.format(elements[0],elements[1]))
-                a, b = int(elements[1]), int(elements[2])
-                c, d = int(elements[3]), int(elements[4])
-                total+=1
-                num = d + 2*c + 4*b + 8*a
-                frecuencias[num]+=1
-        for i in range(len(frecuencias)):
-            frecuencias[i]/=total
-    print(combinations)
-    print(frecuencias)
+                producto_a, producto_b = int(elements[1]), int(elements[2])
+                producto_c, producto_d = int(elements[3]), int(elements[4])
+                total += 1  # Se aumenta el total de elementos
+                # Se opera sobre los productos como con un número binario
+                num = producto_d + 2*producto_c + 4*producto_b + 8*producto_a
+                # Se aumenta la frecuencia de esa combinación
+                frequencies[num] += 1
+    for i in range(len(frequencies)):
+        frequencies[i] /= total
+    print(frequencies)
+    return frequencies
 
 
-#cantidad_compras = relative_freq()
-#bar_graph(cantidad_compras)
-combination_relative_freq('compras.csv')
+def combination_bar_graph(combination_frequencies: list) -> None:
+    """
+    Asignación: 4
+    Realiza un gráfico de barras a partir de una lista
+    de frecuencias relativas
+    param cantidad_compras: lista con la cantidad de compras por producto
+    """
+    titulo = "Frecuencia relativa de la combinación de productos comprados"
+    # Se crean los indices de combinaciones
+    index = ['Nothing', 'D', 'C', 'CD', 'B', 'BD', 'BC', 'BCD',
+             'A', 'AD', 'AC', 'ACD', 'AB', 'ABD', 'ABC', 'ABCD']
+    ''' Agrega la lista de frecuencias relativas de ocurrencia de cada
+    combinación y los ídices al data frame '''
+    df = pd.DataFrame({'Frecuencia de ocurrencia por combinación':
+                       combination_frequencies}, index=index)
+    ax = df.plot.barh()
+    # Se agrega la rotulacion de los ejes y se muestra la figura
+    ax.set_title(titulo)
+    ax.set_xlabel("Frecuencia relativa")
+    ax.set_ylabel("Combinaciones")
+    plt.show()
 
+
+# Se llaman las funciones
+purchase_amount = relative_freq()
+bar_graph(purchase_amount)
+combination_frequencies = combination_relative_freq('compras.csv')
+combination_bar_graph(combination_frequencies)

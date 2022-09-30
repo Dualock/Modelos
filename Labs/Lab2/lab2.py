d14e07ac7f68a9eb1372e83c8df1b4d4dd2e6c24
Dualock <djfonsecamo@gmail.com>
2022-09-30 02:18:21 -0600

    formato pep8 + avance2

--- Labs/Lab2/lab2.py
+++ Labs/Lab2/lab2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tikzplotlib
 import scipy
 from fitter import Fitter
 import pandas as pd
@@ -7,10 +8,10 @@ import matplotlib.pyplot as plt
 from typing import NewType
 
 
-#Declaracion de variables globales y tipo de datos
+# Declaracion de variables globales y tipo de datos
 file = 'arboles.csv'
 global_bins = 30
-dataframe = NewType("dataframe type",pd.DataFrame())
+dataframe = NewType("dataframe type", pd.DataFrame())
 
 
 def cantidad_arboles(df: dataframe) -> None:
@@ -22,7 +23,7 @@ def cantidad_arboles(df: dataframe) -> None:
     """
     # Ordena los datos segun la columna de nombres en orden alfabetico
     df.sort_values(by=['Nombre común'], inplace=True)
-    # Se muestran los primeros y ultimos nombres para verificar que se ordenaron
+    # Se muestran los primeros y ultimos nombres para probar que se ordenaron
     print(df['Nombre común'][0:10])
     # Cuenta la cantidad total de elementos en la columna
     cantidad = df['Nombre común'].count()
@@ -33,7 +34,8 @@ def cantidad_arboles(df: dataframe) -> None:
     print(df['Nombre común'][cantidad-10:cantidad])
     print("cantidad de especies distintas: ", especies_distintas)
     print("Nombre de todas las especes: \n", nombre_especies)
-    
+
+
 def histograma_Arboles(df: dataframe, nombres: list) -> None:
     """
     Asignacion: 2
@@ -64,7 +66,7 @@ def histograma_Arboles(df: dataframe, nombres: list) -> None:
         # nuevo_df: dataframe auxiliar con los datos de un solo tipo de arbol
         nuevo_df = df[df['Nombre común'] == x]
         # Se hace el histograma con 30 divisiones
-        nuevo_df['Diámetro (cm)'].plot(kind='hist', bins = 30)
+        nuevo_df['Diámetro (cm)'].plot(kind='hist', bins=30)
         # Se da formato a los ejes y se muestra la figura
         plt.xlabel('Diametro (cm)')
         plt.ylabel('Frecuencia')
@@ -72,40 +74,37 @@ def histograma_Arboles(df: dataframe, nombres: list) -> None:
         plt.show()
 
 
-def mejor_ajuste(df: dataframe, mis_arboles: list):
+def mejor_ajuste(df: dataframe, mis_arboles: list) -> tuple:
     """
     Asignacion: 3
     Determina los parametros de mejor ajuste de mis 2 arboles
     obtenidos con la funcion asignacion() y se grafica este modelo
     en conjunto con el histograma respectivo
+    param df: dataframe con los datos del archivo arboles.csv
+    param mis_arboles: lista con los arboles asignados con la funcion asign()
     """
-    print(mis_arboles)
+    # para cada nombre en la lista con 2 arboles
     for x in mis_arboles:
         # nuevo_df: dataframe auxiliar con los datos de un solo tipo de arbol
         nuevo_df = df[df['Nombre común'] == x]
-        data = nuevo_df['Diámetro (cm)'].tolist()
-        print(data)
-        f = Fitter(data)
+        # Se transforma de objeto series a un array de numpy
+        data = np.array(nuevo_df['Diámetro (cm)'])
+        # Se utiliza fitter para encontrar los parametros de mejor ajuste
+        f = Fitter(data, distributions=['kappa3', 'skewnorm'])
+        # Realizar el ajuste para las distribuciones seleccionadas
         f.fit()
+        # Mostrar principales resultados y gráfica
         f.summary()
-        print(f.fitted_param['norm'])
-        # Se hace el histograma con 30 divisiones
-        nuevo_df['Diámetro (cm)'].plot(kind='hist', bins = 30)
+        parametros = f.fitted_param
+        print(parametros, type(parametros))
+        archivo = x+".tex"
         # Se da formato a los ejes y se muestra la figura
         plt.xlabel('Diametro (cm)')
         plt.ylabel('Frecuencia')
         plt.title('Histograma de {}'.format(x))
+        # tikzplotlib.save(archivo) # Tira error
         plt.show()
-    
-def test_fitter():
-
-	data = stats.gamma.rvs(2, loc=1.5, scale=2, size=10000)
-	f = Fitter(data, distributions=['gamma', 'rayleigh', 'uniform'])
-	f.fit()
-	# may take some time since by default, all distributions are tried
-	# but you call manually provide a smaller set of distributions
-	f.summary()
-
+    return parametros
 
 
 def asignaciones(digitos):
@@ -118,20 +117,23 @@ def asignaciones(digitos):
     lista = arboles['Nombre común'].unique()
     return (random.choice(lista), random.choices(lista, k=2))
 
+
+# Llamada a funciones
 digitos = 42629
 mis_arboles = asignaciones(digitos)
 arboles_asign3 = [mis_arboles[1][0], mis_arboles[1][1]]
-print('Mis dos árboles son: {} y {}.'.format(arboles_asign3[0], arboles_asign3[1]))
+print('Mis dos árboles son: {} y {}.'.format(arboles_asign3[0],
+                                             arboles_asign3[1]))
 print('Mi árbol es: {}.'.format(mis_arboles[0]))
 arboles = pd.read_csv(file)
 df = pd.DataFrame(arboles)
 ''' Asignacion 1'''
-#cantidad_arboles(df)
+# cantidad_arboles(df)
 
 ''' Asignacion 2'''
 Arboles_histo = ['guapinol', 'balsa', 'gallinazo', 'higuerón', 'malacahuite',
-'mora', 'tamarindo', 'aceituno', 'laurel', 'guarumo']
-#histograma_Arboles(df, Arboles_histo)
+                 'mora', 'tamarindo', 'aceituno', 'laurel', 'guarumo']
+# histograma_Arboles(df, Arboles_histo)
 
 '''Asignacion 3'''
 mejor_ajuste(df, arboles_asign3)
